994d1b0e5c76f1bccf46bd334cd1f63c03269cc0
hafizakbar19 <hafizakbar19@gmail.com>
2023-10-31 14:31:18 +0500

    keys

--- practice/src/components/Lists.jsx
+++ practice/src/components/Lists.jsx
@@ -2,28 +2,33 @@ import React from 'react'
 
 function Lists() {
 
-    // const students = ['Akbar', "Noor", 'Ziarat', 'Ahmed'];
-    // const obj = [
-    //     {
-    //         sName: 'Akbar',
-    //         age: 35,
-    //         sex: 'male'
-    //     },
-    //     {
-    //         sName: 'Noor',
-    //         age: 35,
-    //         sex: 'male'
-    //     },
-    //     {
-    //         sName: "Faiya",
-    //         age: 50,
-    //         sex: 'female'
-    //     }
-    // ];
+    const students = ['Akbar', "Noor", 'Ziarat', 'Ahmed'];
+    const obj = [
+        {
+            sName: 'Akbar',
+            age: 35,
+            sex: 'male'
+        },
+        {
+            sName: 'Noor',
+            age: 35,
+            sex: 'male'
+        },
+        {
+            sName: "Faiya",
+            age: 50,
+            sex: 'female'
+        }
+    ];
+
+
 
   return (
     <div>
         <h3>{students.map(el=>el + " ")}</h3>
+        <div>
+            {students.map((el,index) => <li key={index}>{el}</li>)} {/*adding key is necessry while rendering list items in react*/ }
+        </div>
         <h3>{obj.map(el=>`${el.sName}'s age is ${el.age} and sex is ${el.sex}`)}</h3>
     </div>
   )
